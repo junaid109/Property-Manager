3d3ec99d1115ef78eec037524f3e49d9d13b2ec9
Junaid Malik <junmalik109@gmail.com>
2023-01-13 13:59:36 +0000

    Create MappingConfig.cs

---
+++ PropertyManager.API/MappingConfig.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using PropertyManager.API.Entities;
+using PropertyManager.API.Models;
+
+namespace PropertyManager.API
+{
+	public class MappingConfig: Profile
+	{
+		public MappingConfig()
+		{
+			CreateMap<Property, PropertyDTO>();
+			CreateMap<PropertyDTO, Property>();
+
+			CreateMap<Property, PropertyUpdateDTO>().ReverseMap();
+			CreateMap<PropertyUpdateDTO, Property>();
+			CreateMap<Property, PropertyCreateDTO>().ReverseMap();
+			CreateMap<PropertyCreateDTO, Property>();
+		}
+		
+	}
+}
