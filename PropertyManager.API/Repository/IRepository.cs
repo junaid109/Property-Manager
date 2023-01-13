61a77bf892cf5f4764d41fce1cb6c98364885517
Junaid Malik <junmalik109@gmail.com>
2023-01-13 13:57:40 +0000

    Added IRepository to handle database operations

---
+++ PropertyManager.API/Repository/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using PropertyManager.API.Models;
+using System.Linq.Expressions;
+
+namespace PropertyManager.API.Repository
+{
+	public interface IRepository
+	{
+		// Get all properties
+		IEnumerable<Property> GetAllProperties(Expression<Func<Property>> filter = null);
+		Property GetPropertyAsyn(Expression<Func<Property>> filter = null, bool tracked = false);
+
+		// Get property by id
+		Property GetPropertyById(int id);
+		// Create property
+		void CreateProperty(Property property);
+		// Update property
+		void UpdateProperty(Property property);
+		// Delete property
+		void DeleteProperty(Property property);
+		// Save changes
+		void Save();
+	}
+}
