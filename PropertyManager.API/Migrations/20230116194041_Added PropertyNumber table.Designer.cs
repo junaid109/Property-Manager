b2ce37f6a6e8824fb6bfc2e285833094b11b277d
Junaid Malik <junmalik109@gmail.com>
2023-01-16 19:44:13 +0000

    Added migration for PropertNumber Table

--- PropertyManager.API/Migrations/20230116194041_Added PropertyNumber table.Designer.cs
+++ PropertyManager.API/Migrations/20230116194041_Added PropertyNumber table.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PropertyManager.API.Data;
 
@@ -11,9 +12,11 @@ using PropertyManager.API.Data;
 namespace PropertyManager.API.Migrations
 {
     [DbContext(typeof(PropertyContext))]
-    partial class PropertyContextModelSnapshot : ModelSnapshot
+    [Migration("20230116194041_Added PropertyNumber table")]
+    partial class AddedPropertyNumbertable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PropertyManager.API/Migrations/20230116194041_Added PropertyNumber table.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PropertyManager.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedPropertyNumbertable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PropertyNumber",
+                columns: table => new
+                {
+                    PropertyNum = table.Column<int>(type: "int", nullable: false),
+                    Details = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PropertyNumber", x => x.PropertyNum);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PropertyNumber");
+        }
+    }
+}
