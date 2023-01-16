0bc5865d1e69618cb3cafaf0a373e94b7bf5b017
Junaid Malik <junmalik109@gmail.com>
2023-01-16 19:42:07 +0000

    Api response and property number data models added

---
+++ PropertyManager.API/Models/ApiResponse.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+
+namespace PropertyManager.API.Models
+{
+	public class ApiResponse
+	{
+		public HttpStatusCode StatusCode { get; set; }
+		public bool isSuccess
+		{
+			get; set;
+		}
+		
+		public List<string> Messages { get; set; }
+
+		public object Result { get; set; }
+
+
+	}
+}
---
+++ PropertyManager.API/Models/PropertyNumber.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PropertyManager.API.Models
+{
+	public class PropertyNumber
+	{
+		[Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+		public int PropertyNum { get; set; }
+		public string Details { get; set; }
+
+		public DateTime CreatedDate { get; set; }
+
+		public DateTime UpdatedDate { get; set; }
+
+	}
+}
